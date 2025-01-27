5b19ab087e4fb32b69cb9f69e34faec9bbba6645
Bittu Gupta <guptabittu233230@gmail.com>
2025-01-27 20:57:45 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var orderForm = document.querySelector(".pg-1 .top .order-form")
+var orderBtn = document.querySelector(".pg-1 .top button")
+var orderFormCutBtn = document.querySelector(".pg-1 .top .order-form>i")
+var navOrderBtn = document.querySelector(".pg-1 .nav .right span:nth-child(2)")
+
+
+orderBtn.addEventListener("click",function(){
+    orderForm.style.display = "block";
+});
+
+navOrderBtn.addEventListener("click",function(){
+    orderForm.style.display = "block";
+});
+
+orderFormCutBtn.addEventListener("click",function(){
+    orderForm.style.display = "none";
+});
