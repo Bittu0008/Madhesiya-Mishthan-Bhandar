acc345e15cb8fe92fd70a77baeec531875464e29
Bittu Gupta <guptabittu233230@gmail.com>
2026-01-18 20:08:35 +0530

    index.html style.css script.js added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var orderForm = document.querySelector(".pg-1 .top .order-form")
 var orderBtn = document.querySelector(".pg-1 .top button")
 var orderFormCutBtn = document.querySelector(".pg-1 .top .order-form>i")
-var navOrderBtn = document.querySelector(".pg-1 .nav .right span:nth-child(2)")
+var navOrderBtn = document.querySelector(".pg-1 .nav .right span:nth-child(3)")
 
 
 orderBtn.addEventListener("click",function(){
